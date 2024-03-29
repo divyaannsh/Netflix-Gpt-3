961b66b2034a9b12b57ff27f23866b58dbcac38b
Divyansh <divyanshsrivastav72@gmail.com>
2024-03-29 13:17:34 +0530

    update

--- netflix-gpt/src/Components/Login.js
+++ netflix-gpt/src/Components/Login.js
@@ -17,16 +17,30 @@ const Login = () => {
         /> 
         </div>
      <form className=" w-3/12 absolute p-12 bg-black text-white my-36 mx-auto right-0 left-0 backdrop-opacity-85">
-       <h1 className="font-bold text-3xl py-4">
+       <h1 
+       className="font-bold text-3xl py-4">
+
         {isSignForm ? "Sign In" : "Sign Up"}
         </h1>
-        <input type="text" placeholder="email Address" className="p-4 my-4 w-full bg-gray-600"/>
-        <input type="password" placeholder="Password" className="p-4 my-4 w-full  bg-gray-600"/>
-       
-        <button className="p-4 my-6 bg-red-700 rounded-lg">
+        <input type="text" 
+        placeholder="Email Address"
+         className="p-4 my-4 w-full bg-gray-600"/>
+      {!isSignForm && (
+       <input type="text" 
+        placeholder="Full Name"
+         className="p-4 my-4 w-full bg-gray-600"/>)}
+        
+        <input type="password"
+         placeholder="Password"
+          className="p-4 my-4 w-full  bg-gray-600"/>
+        
+        <button
+         className="p-4 my-6 bg-red-700 rounded-lg">
         {isSignForm ? "Sign In" : "Sign Up"}
             </button>
-        <p className="Py-4 cursor-pointer" onClick={toggleSigninForm}>
+        <p 
+        className="Py-4 cursor-pointer" 
+        onClick={toggleSigninForm}>
         {isSignForm ? " New to Netflix? Sign Up Now" : "You Are already Registered? Sign In Now."}
            
         </p>
